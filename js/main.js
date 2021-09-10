e3092f5f547cabf30c2c4f7eeebb6d0686cf37a1
franklinle <frle@ucsd.edu>
2021-09-10 01:20:21 -0700

    updated some information and removed unneeded html pages

--- js/main.js
+++ js/main.js
@@ -227,22 +227,6 @@ $(function () {
 
   bar.animate(0.65);
 
-  var bar = new ProgressBar.Line(lineprog5, {
-    strokeWidth: 1.72,
-    easing: "easeInOut",
-    duration: 1400,
-    delay: 3200,
-    trailWidth: 1.72,
-    svgStyle: {
-      width: "100%",
-      height: "100%",
-    },
-    step: (state, bar) => {
-      bar.setText(Math.round(bar.value() * 100) + " %");
-    },
-  });
-
-  bar.animate(0.6);
 
   // Contact form
   $(".art-input").keyup(function () {
