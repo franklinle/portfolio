a82adf052484fb00c53c82133a2a4f8a25d5deee
franklinle <frle@ucsd.edu>
2021-09-12 00:08:27 -0700

    linked email to contact form

--- mail.php
+++ mail.php
@@ -1,12 +1,12 @@
 <?php
 
-$recepient = "your.mail@here.com";
-$sitename = "Arter";
+$recepient = "frankle3301@gmail.com";
+$sitename = "franklinle.com";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Name: $name \nEmail: $email \nText: $text";
 
-$pagetitle = "New message from the \"$sitename\"";
+$pagetitle = "New message from \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
